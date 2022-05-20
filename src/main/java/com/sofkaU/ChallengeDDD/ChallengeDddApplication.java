115a746fb921117490680ae610e1602efae1be33
David Ernesto Reina Munar <davidereina@hotmail.com>
2022-05-20 10:30:57 -0500

    Project initialized

---
+++ src/main/java/com/sofkaU/ChallengeDDD/ChallengeDddApplication.java
@@ -0,0 +1,13 @@
+package com.sofkaU.ChallengeDDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeDddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofkaU/ChallengeDDD/ChallengeDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkaU.ChallengeDDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

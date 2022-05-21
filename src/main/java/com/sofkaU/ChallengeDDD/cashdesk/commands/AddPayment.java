e148ded6ee9ababb3a6e28a5fa18bba98b74824b
David Ernesto Reina Munar <davidereina@hotmail.com>
2022-05-21 01:53:03 -0500

    Usecase for addCashier and addPayment implemented

--- src/main/java/com/sofkaU/ChallengeDDD/cashdesk/commands/AddPayment.java
+++ src/main/java/com/sofkaU/ChallengeDDD/cashdesk/commands/AddPayment.java
@@ -1,21 +1,28 @@
 package com.sofkaU.ChallengeDDD.cashdesk.commands;
 
 import co.com.sofka.domain.generic.Command;
+import com.sofkaU.ChallengeDDD.cashdesk.values.CashDeskID;
 import com.sofkaU.ChallengeDDD.cashdesk.values.PaymentID;
 import com.sofkaU.ChallengeDDD.cashdesk.values.PaymentMethod;
 import com.sofkaU.ChallengeDDD.cashdesk.values.Total;
 
 public class AddPayment extends Command {
 
+    private final CashDeskID cashDeskID;
     private final PaymentID paymentID;
     private final PaymentMethod paymentMethod;
     private final Total total;
 
 
-    public AddPayment(PaymentID paymentID, PaymentMethod paymentMethod, Total total) {
+    public AddPayment(CashDeskID cashDeskID, PaymentID paymentID, PaymentMethod paymentMethod, Total total) {
+        this.cashDeskID = cashDeskID;
         this.paymentID = paymentID;
         this.paymentMethod = paymentMethod;
         this.total = total;
+    }
+
+    public CashDeskID cashDeskID() {
+        return cashDeskID;
     }
 
     public PaymentID paymentID() {
--- src/main/java/com/sofkaU/ChallengeDDD/cashdesk/usecase/AddPaymentUseCase.java
+++ src/main/java/com/sofkaU/ChallengeDDD/cashdesk/usecase/AddPaymentUseCase.java
@@ -4,15 +4,15 @@ import co.com.sofka.business.generic.UseCase;
 import co.com.sofka.business.support.RequestCommand;
 import co.com.sofka.business.support.ResponseEvents;
 import com.sofkaU.ChallengeDDD.cashdesk.CashDesk;
-import com.sofkaU.ChallengeDDD.cashdesk.commands.AddCashier;
+import com.sofkaU.ChallengeDDD.cashdesk.commands.AddPayment;
 
-public class AddCashierUseCase extends UseCase<RequestCommand<AddCashier>, ResponseEvents> {
+public class AddPaymentUseCase extends UseCase<RequestCommand<AddPayment>, ResponseEvents>{
 
     @Override
-    public void executeUseCase(RequestCommand<AddCashier> addCashierRequestCommand) {
-        var command = addCashierRequestCommand.getCommand();
+    public void executeUseCase(RequestCommand<AddPayment> addPaymentRequestCommand) {
+        var command = addPaymentRequestCommand.getCommand();
         var cashdesk = CashDesk.from(command.cashDeskID(), retrieveEvents(command.cashDeskID().value()));
-        cashdesk.addCashier(command.cashierID(), command.name(), command.yearsOfExperience());
+        cashdesk.addPayment(command.paymentID(), command.paymentMethod(), command.total());
 
         emit().onResponse(new ResponseEvents(cashdesk.getUncommittedChanges()));
     }
